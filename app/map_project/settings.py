176e1e0a25a92cde93efc16bb345cf838858b158
3ANov <3anovdev@gmail.com>
2020-11-01 00:37:15 +0300

    Редактирую шаблоны + настройки

--- app/map_project/settings.py
+++ app/map_project/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'bootstrap4',
     'users',
     'map',
 ]
@@ -81,6 +82,12 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #AUTH_USER_MODEL = 'users.User'
 
