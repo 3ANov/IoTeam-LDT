df0f8e9cf57193d969faff00c0a17cb35cf5453f
3ANov <3anovdev@gmail.com>
2020-10-31 15:27:39 +0300

    Добавил модель записи

--- app/map/models.py
+++ app/map/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class OdhRecord(models.Model):
+    """
+    Объект - который хранит записи из odh geojson, но без большнства полей
+        root_id - Идентификатор ОДХ
+        name - Наименование объекта
+        geometry - пространственные координаты объекта в виде строки
+        geometry_type - тип пространственного объекта (Точка, полигон и т.д.)
+    """
+    root_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=300)
+    geometry = models.TextField()
+    geometry_type = models.CharField(max_length=20)
--- app/map/views.py
+++ app/map/views.py
@@ -1,5 +1,9 @@
+from django.core.serializers import serialize
 from django.views.generic import TemplateView
+
+from map.models import OdhRecord
 
 
 class MapTemplateView(TemplateView):
     template_name = "map/map.html"
+    context = serialize('geojson', OdhRecord.objects.all())
