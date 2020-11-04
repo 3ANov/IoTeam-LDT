a0e40bacc619c2d16ff4eb292919cb5304679ed8
3ANov <3anovdev@gmail.com>
2020-11-04 11:05:50 +0300

    Правлю добавление в базу

--- app/file_processing/services.py
+++ app/file_processing/services.py
@@ -1,55 +1,35 @@
-#  https://t.me/elemnt3 Y:2020
-
 import json
 
-# creat example for TXT FORMAT FILES file and correct quickly
-# creat
 from file_processing.models import FileModel
+from map.models import OdhRecord
 
-print(FileModel.objects.get(content_type='ODX_DATA'))
-with open('odh20200928', 'r', encoding='utf-8') as f:
-    lines = f.read().split('\n')
-js = json.loads(lines[0])
-with open('data_from_json.txt', 'w', encoding='utf-8') as f:
-    """Name, Passport_draft_org, Geometry"""
-    print('Count:', len(lines), file=f)
-    try:
+
+def parsing_odh_file():
+    if FileModel.objects.filter(content_type='ODH_DATA').first() is not None:
+        f = FileModel.objects.filter(content_type='ODH_DATA').first()
+        lines = f.content.open('r').read().split('\n')
         for i in range((len(lines)) - 1):
-            print('Name: ', (json.loads(lines[i])['name']), file=f)
-            if 'passport_draft_org' in lines[i]:
-                print('Passport_draft_org:', (json.loads(lines[i])['passport_draft_org']),
-                      file=f)
+            if 'name' in lines[i]:
+                id = i + 1
+                name = json.loads(lines[i])['name']
+                root_id = json.loads(lines[i])['root_id']
+                object_id = json.loads(lines[i])['object_id']
+                distance = json.loads(lines[i])['distance']
             if 'geometry' in lines[i]:
-                print('Geometry', (json.loads(lines[i])['geometry']), file=f)
-            print('===========================================================================', file=f)
-    except IndexError as err:
-        print(err)
-# ALL 7596
-# 19
-tags = ["name", "tree", "root_id", "distance", "end_date", "geometry", "owner_id", "file_list", "object_id",
-        "start_date", "customer_id", "department_id", "passport_date", "calc_attribute", "declare_length",
-        "is_orphan_object", "clean_category_id", "ogh_object_type_id", "passport_draft_org"]
-# 23
-all_tags = ['name',
-            'tree',
-            'root_id',
-            'distance',
-            'end_date',
-            'geometry',
-            'owner_id',
-            'file_list',
-            'object_id',
-            'snow_area',
-            'rotor_area',
-            'start_date',
-            'axis_length',
-            'customer_id',
-            'reagent_area',
-            'department_id',
-            'layout_length',
-            'passport_date',
-            'calc_attribute',
-            'is_orphan_object',
-            'clean_category_id',
-            'ogh_object_type_id',
-            'passport_draft_org']
+                geometry = str(json.loads(lines[i])['geometry'])  # all data
+                if json.loads(lines[i])['geometry']['type'] == 'GeometryCollection':
+                    geometry_type = str(json.loads(lines[i])['geometry']['geometries'][1]['type'])
+                    coordinates = str(json.loads(lines[i])['geometry']['geometries'][1]['coordinates'])
+                elif json.loads(lines[i])['geometry']['type'] != 'GeometryCollection':
+                    geometry_type = str(json.loads(lines[i])['geometry']['type'])
+                    coordinates = str(json.loads(lines[i])['geometry']['coordinates'])
+
+            if 'geometry' not in lines[i]:
+                geometry = 0
+            OdhRecord.objects.create(root_id=root_id,
+                                     distance=distance,
+                                     object_id=object_id,
+                                     name=name,
+                                     geometry=geometry,
+                                     geometry_type=geometry_type,
+                                     coordinates=coordinates)
--- app/file_processing/views.py
+++ app/file_processing/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.files import File
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
@@ -6,6 +7,7 @@ from django.views import View
 
 from file_processing.forms import FileUploadForm
 from file_processing.models import FileModel
+from file_processing.services import parsing_odh_file
 
 
 class FileUploadView(LoginRequiredMixin, View):
@@ -21,6 +23,7 @@ class FileUploadView(LoginRequiredMixin, View):
         form = self.form_class(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+            #parsing_odh_file()
             return redirect(self.success_url)
         else:
             return render(request, self.template_name, {'form': form})
@@ -29,4 +32,5 @@ class FileUploadView(LoginRequiredMixin, View):
 def file_view(request):
     file_to_read = FileModel.objects.filter(content_type='ODH_DATA').first()
     out_str = file_to_read.content.open('r').readlines()
+    file_to_read.content.close()
     return render(request, 'file_processing/file_read.html', {'out_str': out_str})
